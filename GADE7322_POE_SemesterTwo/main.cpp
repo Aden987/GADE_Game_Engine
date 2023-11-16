0c5a5008b608d83cca53e7534841c4bb55f41ead
Aden987 <naidoo.aden@gmail.com>
2023-11-16 14:22:03 +0200

    Added skybox

--- GADE7322_POE_SemesterTwo/main.cpp
+++ GADE7322_POE_SemesterTwo/main.cpp
@@ -24,6 +24,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
+unsigned int loadCubemap(vector<std::filesystem::path> faces);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -100,6 +101,7 @@ int main()
     Shader myShader("resources/shaders/basic.shader.vert","resources/shaders/basic.shader.frag");
     Shader heightMapShader("resources/shaders/heightmap.shader.vert", "resources/shaders/heightmap.shader.frag");
     Shader importShader("resources/shaders/importBasic.shader.vert", "resources/shaders/importBasic.shader.frag");
+    Shader skyboxShader("resources/shaders/skybox.shader.vert", "resources/shaders/skybox.shader.frag");
 
     //load texture
 #pragma region TEXTURE
@@ -304,6 +306,34 @@ int main()
     myShader.setInt("texture7", 6);
 #pragma endregion
 
+    //skybox texture
+    std::filesystem::path rightMap = "resources/textures/skybox/right.jpg";
+    std::filesystem::path leftMap = "resources/textures/skybox/left.jpg";
+    std::filesystem::path topMap = "resources/textures/skybox/top.jpg";
+    std::filesystem::path bottomMap = "resources/textures/skybox/bottom.jpg";
+    std::filesystem::path frontMap = "resources/textures/skybox/front.jpg";
+    std::filesystem::path backMap = "resources/textures/skybox/back.jpg";
+
+    /*std::filesystem::path rightMap = "resources/textures/newSkybox/right.jpg";
+    std::filesystem::path leftMap = "resources/textures/newSkybox/left.jpg";
+    std::filesystem::path topMap = "resources/textures/newSkybox/top.jpg";
+    std::filesystem::path bottomMap = "resources/textures/newSkybox/bottom.jpg";
+    std::filesystem::path frontMap = "resources/textures/newSkybox/front.jpg";
+    std::filesystem::path backMap = "resources/textures/newSkybox/back.jpg";*/
+
+    vector<std::filesystem::path> faces
+    {
+        rightMap,
+        leftMap,
+        topMap,
+        bottomMap,
+        frontMap,
+        backMap
+    };
+    unsigned int cubemapTexture = loadCubemap(faces);
+    //skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
+
 #pragma region HeightMapTexture
     unsigned int texture1;
 
@@ -430,6 +460,51 @@ int main()
 
     basicCubeMesh myCube(vertices);
 
+    float skyboxVertices[] = {
+        // positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
+    };
+
     //cylinder model properties
     float cylinderRadius = 0.5f;
     float cylinderHeight = 1.0f;
@@ -466,6 +541,16 @@ int main()
     glGenBuffers(1, &terrainEBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrainEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    // skybox VAO
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     AnimationController anim;
 
@@ -517,11 +602,11 @@ int main()
         }
 
         
-
-        myShader.use();
-
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100000.0f);
         glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 skyView = camera.GetViewMatrix();
+
+        myShader.use();
         myShader.setMat4("projection", projection);
         myShader.setMat4("view", view);
        
@@ -1102,6 +1187,23 @@ int main()
         }
 
 #pragma endregion
+
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        //glEnable(GL_DEPTH_CLAMP);
+        skyboxShader.use();
+        glm::mat4 skyModel = glm::mat4(1.0f);
+        skyView = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
+        skyboxShader.setMat4("view", skyView);
+        skyboxShader.setMat4("projection", projection);
+        skyboxShader.setMat4("model", skyModel);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -1254,4 +1356,42 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
+}
+
+unsigned int loadCubemap(vector<std::filesystem::path> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(false);
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].generic_string().c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            GLenum format;
+            if (nrChannels == 1)
+                format = GL_RED;
+            else if (nrChannels == 3)
+                format = GL_RGB;
+            else if (nrChannels == 4)
+                format = GL_RGBA;
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
